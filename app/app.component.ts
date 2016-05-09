5291c5ffc9919f1e83e148e0d7565c3051ecd415
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2016-05-09 16:07:38 -0700

    Angular2 Seed Project

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+import {CoursesComponent} from './courses.component';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <courses></courses>
+  `,
+  directives: [CoursesComponent]
+})
+export class AppComponent{
+  title = "Angular 2 Starting over";
+
+}
