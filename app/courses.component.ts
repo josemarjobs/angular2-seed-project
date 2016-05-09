d4ccf65d159d009a892dc55a4b1f4e4026050943
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2016-05-09 16:29:13 -0700

    Add A service

--- app/courses.component.ts
+++ app/courses.component.ts
@@ -1,9 +1,23 @@
 import {Component} from 'angular2/core';
+import {CourseService} from './course.service';
 
 @Component({
   selector: 'courses',
-  template: `<h2>Courses</h2>`
+  template: `
+    <h2>Courses</h2>
+    <p>{{title}}</p>
+
+    <ul>
+      <li *ngFor="#course of courses">{{course}}</li>
+    </ul>
+  `,
+  providers: [CourseService]
 })
 export class CoursesComponent {
+  title = "The title of the courses";
+  courses: string [];
 
+  constructor(courseService: CourseService) {
+    this.courses = courseService.getCourses();
+  }
 }
